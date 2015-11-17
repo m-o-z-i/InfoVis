7abf2906281de46314f3cc0842a35d1c250754bf
Moritz Loos <moritz.loos@uni-weimar.de>
2015-11-17 20:50:40 +0100

    clean code.

--- script.js
+++ script.js
@@ -3,26 +3,14 @@ var vis = {
     width: 800,
     height: 800,
     
-    donutWidth: 75,
-
-    legendWidth: 300,
-    legendRectSize: 18,
-    legendSpacing: 4
+    donutWidth: 75
 }
 
 var radius = Math.min(vis.width, vis.height) / 2;
-var arcLength = d3.scale.linear()
-    .range([0, 2 * Math.PI]);
-var arcDistance = d3.scale.linear()
-    .range([0, radius]);
+var arcLength = d3.scale.linear().range([0, 2 * Math.PI]);
+var arcDistance = d3.scale.linear().range([0, radius]);
 
-var x = d3.scale.linear().range([0, 2 * Math.PI]);
-var y = d3.scale.pow().exponent(1).domain([0, 1]).range([0, radius]);
-var padding = 10;
 var duration = 1000;
-
-// own scale ..     
-var color = d3.scale.category20b();
 
 // define partition size
 var partition = d3.layout.partition()
@@ -40,22 +28,23 @@ var arc = d3.svg.arc()
 // load and process data
 d3.json("asyl.json", function(error, data) {
     if (error) throw error;
-
     draw(data);
 });
 
 
 // define svg element, that is locatet in the middle of diagram
-var svg = d3.select('#chart')
+var svg = d3.select('#cirlceSet')
     .append("svg")
-    .attr("width", vis.width + vis.legendWidth)
+    .attr("width", vis.width)
     .attr("height", vis.height)
     .append("g");
+svg.attr("transform", "translate(" + vis.width / 2 + "," + (vis.height / 2) + ")");
 
-
-//arcPart group
+// add slices to DOM
 svg.append("g")
     .attr('class', "slices");
+
+// add labels to DOM
 svg.append("g")
     .attr('class', "labels");
 
@@ -69,9 +58,6 @@ labels = arcPartGroup.append("labels")
     .attr('class', "labels");*/
 
 
-svg.attr("transform", "translate(" + vis.width / 2 + "," + (vis.height / 2) + ")");
-
-
 function draw(data)
 {
     var totalSize = data.size;
@@ -79,9 +65,8 @@ function draw(data)
     // create arc visualisation
     console.log("call draw method:");
 
-
     // *********************    tooltip / infobox   ******************************** //
-    var tooltip = d3.select('#chart')
+    var tooltip = d3.select('#cirlceSet')
         .append('div')
         .attr('class', 'tooltip');
     tooltip.append('div')
@@ -98,15 +83,13 @@ function draw(data)
     var slice = svg.select(".slices").selectAll("path.slice")
         .data(partition.nodes(data));
 
-
     slice.enter()
         .insert("path")
-        .style("fill", function(d) { return color(d.name); })
+        //.style("fill", function(d) { return color(d.name); })
         .attr('class', "slice")
         .attr("id", function(d, i) { return "path-" + i; })
         .attr("d", arc)
-        .attr("fill-rule", "evenodd")
-        .on("click", clickSlice);
+        .attr('display', function(d, i) {if(i==0) return "none";});
 
     slice.on('mouseover', function(d) {
         var parentSize = (typeof d.parent == "undefined") ? d.value : d.parent.size ;
@@ -143,54 +126,28 @@ function draw(data)
     var text = svg.select(".labels").selectAll("text")
         .data(partition.nodes(data));
 
-    text.enter()
-        .append("text")
+  var textEnter = text.enter().append("text")
+        .attr('class', function(d, i) {
+            if (i == 0) return "title";
+            else return "label";
+        })
         .attr("dy", ".35em")
         .attr("id", function(d, i) { return "label-" + i; })
-        .attr("display", function(d) { if (d.size ==  0) return "none";})
-        .style("text-anchor", "middle")
-        .attr("transform", function(d) { 
-          return "translate(" + arc.centroid(d) + ")"; 
-        })
-        .text(function(d) { return d.name; });
+        .attr("display", function(d, i) { if (d.size ==  0) return "none";})
+        .attr("transform", function(d, i) { 
+            if (i != 0) return "translate(" + arc.centroid(d) + ")";
+        });
 
-    function midAngle(d){
-        return d.startAngle + (d.endAngle - d.startAngle)/2;
-    }
+  textEnter.append("tspan")
+      .attr("x", 0)
+      .text(function(d) { return d.name; });
+  textEnter.append("tspan")
+      .attr("x", 0)
+      .attr("dy", "1em")
+      .text(function(d, i) { if (i==0) return d.size; });
 
     text.exit()
         .remove();
     // ***************************************************************************** //
-
-    function updateSlices(d){
-        slice.transition()
-        .duration(duration)
-        .attrTween("d", arcTween(d))
-        .each("end", updateLabels);
-    }
-    function updateLabels(d){
-        text.transition().duration(duration)
-        .attr("transform", function(d) { 
-          return "translate(" + arc.centroid(d) + ")"; 
-        })
-        .attr("display", function(d) {if (this.x < 0.1 && this.y > -0.1) return "none";});
-    }
-
-    function clickSlice(d){
-        updateSlices(d);
-        updateLabels(d);
-    }
-}
-
-// Interpolate the scales!
-function arcTween(d) {
-  var xd = d3.interpolate(arcLength.domain(), [d.x, d.x + d.dx]),
-      yd = d3.interpolate(arcDistance.domain(), [d.y, 1]),
-      yr = d3.interpolate(arcDistance.range(), [d.y ? 20 : 0, radius]);
-  return function(d, i) {
-    return i
-        ? function(t) { return arc(d); }
-        : function(t) { arcLength.domain(xd(t)); arcDistance.domain(yd(t)).range(yr(t)); return arc(d); };
-  };
 }
 
